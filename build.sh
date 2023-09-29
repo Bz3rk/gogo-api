2dc302812db0c551d89ca45ea0614795986a27e7
B-zerk <anthonytayo155@gmail.com>
2023-09-29 19:24:31 +0100

    adding all changes to bash

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
-pip install -r requirements.txt
+pip install coreapi
+pip install coreschema
+pip install Django
+pip django-cors-headers
+pip djangorestframework
+pip install geopy
+pip install gunicorn
+pip install Pillow
+pip install PyYAML
+pip install whitenoise
 python manage.py makemigrations
 python manage.py migrate
