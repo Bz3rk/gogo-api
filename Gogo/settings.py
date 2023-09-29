15afd0df5459f3bc449c9ccef83276e338739105
Bz3rk <121658460+Bz3rk@users.noreply.github.com>
2023-09-29 15:26:32 +0100

    Update settings.py

--- Gogo/settings.py
+++ Gogo/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'Gogo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gogodatabase',
+        'HOST': 'dpg-ckbdqhfs0fgc73f93epg-a',
+        'USERNAME': 'gogouser',
+        'PASSWORD': '2KemeBHLfxNG0jP6GVgPgpTN9ua6Dx6M',
+        'PORT': '5432',
     }
 }
 
