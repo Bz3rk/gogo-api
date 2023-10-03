d5668c9399b2ef57a43c918422f239298387e3dd
B-zerk <anthonytayo155@gmail.com>
2023-10-03 17:28:53 +0100

    added some other origins with all path true seeeh

--- Gogo/settings.py
+++ Gogo/settings.py
@@ -121,12 +121,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS: True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "https://gogo-rides-test.onrender.com"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "https://gogo-rides-test.onrender.com"
+# ]
+
+
 
 
 # Internationalization
