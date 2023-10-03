5997c52e2fc841d638194ce21ee3aeea3d67dfec
B-zerk <anthonytayo155@gmail.com>
2023-10-03 14:01:39 +0100

    origin

--- Gogo/settings.py
+++ Gogo/settings.py
@@ -42,12 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'Testimonials',
     'booking_summary',
     'registration',
     'rest_framework',
-    'rest_framework.authtoken',
-    'corsheaders',
+    'rest_framework.authtoken'
+ 
 ]
 
 
@@ -120,7 +121,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 
 # Internationalization
