ac2a4945f1267abf7f1a4403be4e463e955ff369
B-zerk <anthonytayo155@gmail.com>
2023-10-17 13:37:03 +0100

    Feature: updated settings, views, schema

--- Gogo/settings.py
+++ Gogo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
- DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG')
 
 #ALLOWED_HOSTS = ['gogo-api-kpu0.onrender.com']
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
