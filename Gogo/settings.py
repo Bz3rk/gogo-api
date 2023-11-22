3d8763f8e2c6471d1bb220ad20ee0cb24a5133e1
B-zerk <anthonytayo155@gmail.com>
2023-11-23 00:40:42 +0100

    added rest framework defaults

--- Gogo/settings.py
+++ Gogo/settings.py
@@ -55,6 +55,14 @@ INSTALLED_APPS = [
     'registration', 
 ]
 
+#rest framework defaults
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'GOGO-API',
@@ -183,12 +191,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-
-}
 
 
