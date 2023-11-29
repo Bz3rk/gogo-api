7c3c031a6123ee48b63deb4ef1cd35024e574010
B-zerk <anthonytayo155@gmail.com>
2023-11-29 18:18:39 +0100

    Feature: fixing static files bug check 5

--- Gogo/settings.py
+++ Gogo/settings.py
@@ -37,22 +37,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-q4g^7=r2*nne*$-x4d74q=-^ut5@x(i-1^7*icxo7fx9r7#qpp'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-q4g^7=r2*nne*$-x4d74q=-^ut5@x(i-1^7*icxo7fx9r7#qpp'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # DEBUG = os.environ.get('DEBUG')
 
-# ALLOWED_HOSTS = ['gogo-api-kpu0.onrender.com', '127.0.0.1', 'localhost']
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ['gogo-api-kpu0.onrender.com', '127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 AUTH_USER_MODEL = 'registration.CustomUser'
 # Application definition
 
 
 TWILIO_ACCOUNT_SID = 'AC5a34cbe0c6bfe10f325476b624563da9'
-TWILIO_AUTH_TOKEN = '5330a2750efc11a8df72c9f82c98588d'
+TWILIO_AUTH_TOKEN = '4d37c7afa42311bf1128ead124abd416'
 TWILIO_PHONE_NUMBER = '+12677992249'
 
 
@@ -138,15 +138,15 @@ WSGI_APPLICATION = 'Gogo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gogodatabase',
-        'HOST': 'dpg-ckbdqhfs0fgc73f93epg-a',
-        # 'HOST': 'dpg-ckbdqhfs0fgc73f93epg-a.oregon-postgres.render.com',
-        'USER': 'gogouser',
-        'PASSWORD': '2KemeBHLfxNG0jP6GVgPgpTN9ua6Dx6M',
-        'PORT': '5432',
-        # 'ENGINE': 'django.db.backends.sqlite3', 
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'gogodatabase',
+        # 'HOST': 'dpg-ckbdqhfs0fgc73f93epg-a',
+        # # 'HOST': 'dpg-ckbdqhfs0fgc73f93epg-a.oregon-postgres.render.com',
+        # 'USER': 'gogouser',
+        # 'PASSWORD': '2KemeBHLfxNG0jP6GVgPgpTN9ua6Dx6M',
+        # 'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     } 
 }
 
