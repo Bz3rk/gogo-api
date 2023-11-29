18ccbe5a9023f5a986fc35abce12404ef7716b78
B-zerk <anthonytayo155@gmail.com>
2023-11-29 16:27:29 +0100

    Feature: fixing static files bug check 1

--- Gogo/settings.py
+++ Gogo/settings.py
@@ -207,7 +207,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '')
+# STATIC_ROOT = os.path.join(BASE_DIR, '')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
