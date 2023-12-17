efc0262f132e269cc3cb3eaad882cd7c447ed468
B-zerk <anthonytayo155@gmail.com>
2023-12-17 17:04:41 +0100

    push 6 for requirements.txt

--- build.sh
+++ build.sh
@@ -1,25 +1,11 @@
 pip install --upgrade pip
-# pip install asgiref
-# pip install coreapi
-# pip install Django
-# pip install django-cors-headers
-# pip install djangorestframework
-# pip install django-rest-swagger
-# pip install drf-yasg
-# pip install drf-spectacular
-# pip install geopy
-# pip install gunicorn
-# pip install Pillow
-# pip install psycopg2-binary
-# pip install pytz
-# pip install PyYAML
-# pip install requests
-# pip install sqlparse
-# pip install typing_extensions
-# pip install tzdata
-# pip install whitenoise
-# pip install django-cors-headers
-# pip install twilio
+
+
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+
 pip install -r requirements.txt
 python manage.py spectacular --file schema.yml
 echo yes | python manage.py collectstatic
