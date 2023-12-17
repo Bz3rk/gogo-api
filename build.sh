58b4dc1fb4bb8db6780cb09e769a5546c5322a74
B-zerk <anthonytayo155@gmail.com>
2023-12-17 16:45:47 +0100

    Tried to ignore the pycache

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
 pip install --upgrade pip
-pip install asgiref
-pip install coreapi
-pip install Django
-pip install django-cors-headers
-pip install djangorestframework
-pip install django-rest-swagger
-pip install drf-yasg
-pip install drf-spectacular
-pip install geopy
-pip install gunicorn
-pip install Pillow
-pip install psycopg2-binary
-pip install pytz
-pip install PyYAML
-pip install requests
-pip install sqlparse
-pip install typing_extensions
-pip install tzdata
-pip install whitenoise
-pip install django-cors-headers
-pip install twilio
+# pip install asgiref
+# pip install coreapi
+# pip install Django
+# pip install django-cors-headers
+# pip install djangorestframework
+# pip install django-rest-swagger
+# pip install drf-yasg
+# pip install drf-spectacular
+# pip install geopy
+# pip install gunicorn
+# pip install Pillow
+# pip install psycopg2-binary
+# pip install pytz
+# pip install PyYAML
+# pip install requests
+# pip install sqlparse
+# pip install typing_extensions
+# pip install tzdata
+# pip install whitenoise
+# pip install django-cors-headers
+# pip install twilio
 python manage.py spectacular --file schema.yml
 echo yes | python manage.py collectstatic
 python manage.py makemigrations
